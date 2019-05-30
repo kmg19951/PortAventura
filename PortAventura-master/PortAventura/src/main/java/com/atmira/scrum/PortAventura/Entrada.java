d5a934e9ab4920a2d197ee29a6a2a71dbfa81264
Joan Pere <joanpee19@gmail.com>
2019-05-30 13:01:33 +0200

    feature/HistoriaUsuario_001
    Creacion clases cliente, entrada y reserva

---
+++ PortAventura-master/PortAventura/src/main/java/com/atmira/scrum/PortAventura/Entrada.java
@@ -0,0 +1,34 @@
+package com.atmira.Exercicis.ParcAtraccions;
+
+public class Entrada {
+
+	private String fecha;
+	private double precio;
+
+	public String getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public Entrada(String fecha, double precio) {
+		super();
+		this.fecha = fecha;
+		this.precio = precio;
+	}
+
+	public Entrada() {
+		super();
+	}
+
+}
