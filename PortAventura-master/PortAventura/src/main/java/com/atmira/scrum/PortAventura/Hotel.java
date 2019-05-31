52dbd92aae0ae9307a4b297206f2cad2e2882684
AM-VIL-POR-206$ <AM-VIL-POR-206$@AM-VIL-POR-206.atmira.global>
2019-05-31 12:00:24 +0200

    feature/HistoriaUsuario012
    Relacion hotel>habitacion>reserva
    
    Conflicts:
    	PortAventura-master/PortAventura/src/main/java/com/atmira/scrum/PortAventura/App.java
    	PortAventura-master/PortAventura/src/main/java/com/atmira/scrum/PortAventura/Habitacion.java
    	PortAventura-master/PortAventura/src/main/java/com/atmira/scrum/PortAventura/Hotel.java

--- PortAventura-master/PortAventura/src/main/java/com/atmira/scrum/PortAventura/Hotel.java
+++ PortAventura-master/PortAventura/src/main/java/com/atmira/scrum/PortAventura/Hotel.java
@@ -1,13 +1,18 @@
 package com.atmira.scrum.PortAventura;
 
+import java.util.ArrayList;
+
 public class Hotel {
+	//Welcome
 	private int numHab;
 	private String nombre;
+	private ArrayList<Habitacion> listaDeHabitaciones = new ArrayList<Habitacion>();
 	
 	public Hotel(int numHab, String nombre) {
 		super();
 		this.numHab = numHab;
 		this.nombre = nombre;
+		crearHabitaciones();
 	}
 	
 	public int getNumHab() {
@@ -23,6 +28,34 @@ public class Hotel {
 		this.nombre = nombre;
 	}
 	
-	//TEST 
+	
+
+	public ArrayList<Habitacion> getListaDeHabitaciones() {
+		return listaDeHabitaciones;
+	}
+
+	private void crearHabitaciones() {
+		
+		Habitacion h;
+		int contador = 1;
+		
+		for (int i = 0; i < numHab; i++) {
+			if(contador>3) {
+				h = new Habitacion(6.0, i+1, false);
+				listaDeHabitaciones.add(h);
+				contador = 0;
+				
+			}else {
+				h = new Habitacion(3.0, i+1, false);
+				listaDeHabitaciones.add(h);
+				contador = contador + 1;
+			}
+		
+		}
+	}
+
+	
+	
+	
 
 }
