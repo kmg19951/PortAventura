28f8b44f95184cfcf9dcaa74ff96e16186177036
jcandalijash <javier.can.rod@gmail.com>
2019-05-31 09:46:22 +0200

    feature/HistoriaUsuario010
    Estructura de la clase Parque

--- PortAventura-master/PortAventura/src/main/java/com/atmira/scrum/PortAventura/Parque.java
+++ PortAventura-master/PortAventura/src/main/java/com/atmira/scrum/PortAventura/Parque.java
@@ -1,34 +1,18 @@
 package com.atmira.scrum.PortAventura;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 
 public class Parque {
-
-	private final static String FICHERO_INFO_PARQUE_NUEVO = "InformacionParque/InfoParque.txt";
-	private String nombre = "Port Aventura";
-	private ArrayList<Zona> listaDeZonas = new ArrayList<Zona>();
-
-	// Constructores
-	public Parque() {
-		super();
-	}
+	
+	private final String nombre = "Cecilio Aventura";
+	private ArrayList<Zona> listaDeZonas;
 
 	public Parque(String nombre, ArrayList<Zona> listaDeZonas) {
-		super();
-		this.nombre = nombre;
-		this.listaDeZonas = listaDeZonas;
+		this.listaDeZonas = new ArrayList<Zona>();
 	}
 
-	// Getters and Setters
 	public String getNombre() {
 		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
 	}
 
 	public ArrayList<Zona> getListaDeZonas() {
@@ -42,40 +26,6 @@ public class Parque {
 	@Override
 	public String toString() {
 		return "Parque [listaDeZonas=" + listaDeZonas + ", nombre=" + nombre + "]";
-	}
-
-	public void escribirInfoParque() {
-
-		if (this.nombre != null && !("".equals((this.nombre).trim()))) {
-			try {
-				FileWriter fichero = new FileWriter(FICHERO_INFO_PARQUE_NUEVO);
-				BufferedWriter escribirFichero = new BufferedWriter(fichero);
-				escribirFichero.write("Nombre:" + this.nombre);
-				escribirFichero.newLine();
-				if (this.listaDeZonas.size() > 0) {
-					escribirFichero.write("Lista de zonas:");
-					for (Zona zona : getListaDeZonas()) {
-						escribirFichero.newLine();
-						escribirFichero.write("Nombre zona:" + zona.getNombre());
-						escribirFichero.newLine();
-						escribirFichero.write("Descripcion zona:" + zona.getDescripcion());
-						escribirFichero.newLine();
-						escribirFichero.write("Color zona:" + zona.getColor());
-						escribirFichero.newLine();
-						// escribirFichero.write("Hotel
-						// zona:"+zona.getHotel().getNombre());
-
-					}
-				}
-
-				escribirFichero.close();
-
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-
 	}
 
 }
